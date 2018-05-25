f5f0b45c0408de2b6533f4e752ab6029539ab562
Sébastien Jacques <sebastien@X-Wing.local>
2018-05-25 15:57:19 +0200

    Add comments

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -7,12 +7,12 @@ namespace Classes
     {
         static void Main(string[] args)
         {
-            var user = new Person();
+            var user = new Person();    // No need to import if it's in the same namespace
             user.FirstName = "John";
             user.LastName = "Doe";
             user.Introduce();
 
-            var calc = new Calculator(); // use custom method from another namespace
+            var calc = new Calculator(); // use custom method from another namespace => need import from Classes.CustomMath
 
             Console.WriteLine(calc.Add(1, 2));
         }
