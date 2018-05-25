f85fdcc7de1839e6821f46a04a48ff1ba82369e1
Sébastien Jacques <sebastien@X-Wing.local>
2018-05-25 15:00:58 +0200

    Test some operators

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -6,7 +6,25 @@ namespace Operators
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a / b); // a and b are int sor the result is int
+            Console.WriteLine((float) a / (float) b);   // cast in float so result is in float 
+
+            Console.WriteLine(a + b * c);   // c# take care of operations priority (first b*c, then + a)
+            Console.WriteLine((a + b) * c);   // use () like in math to change priorities
+            
+            Console.WriteLine(a > b);   // false
+            Console.WriteLine(a == b);   // false
+            Console.WriteLine(a != b);   // true
+            Console.WriteLine(!(a != b));   // false  => a == b
+
+            Console.WriteLine(c > b && c > a);   // true & true == true
+
+            Console.WriteLine(c > b || c == a);   // true || false == true
         }
     }
 }
