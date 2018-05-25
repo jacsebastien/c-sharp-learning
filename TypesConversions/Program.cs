aa0ac684a8c9e1b2cc975a4fc14e662718c5ba50
Sébastien Jacques <sebastien@X-Wing.local>
2018-05-25 14:46:29 +0200

    Demosntrate conventions

--- TypesConversions/Program.cs
+++ TypesConversions/Program.cs
@@ -6,7 +6,34 @@ namespace TypesConversions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            byte b = 1;
+            int i = b;  // Ok byte is shorten than int
+            
+            int a = 2;
+            // byte c = a; // Error int is larger than byte
+
+            byte c = (byte) a; // Ok because of cast but data can be lost if greater than byte
+
+            var number = "1234";    // is detect as a string
+            // int i = (int) number;    // cant cast string to int
+            int intNumber = Convert.ToInt32(number);    // Ok
+            // byte byteNumber = Convert.ToByte(number);   // Pass build process but app will crash because byte is too small to contain 1234
+          
+            // Console.WriteLine(intNumber);
+            
+            // Prevent from crashing
+            try
+            {
+                var numberTry= "1234";    // is detect as a string
+                byte byteNumberTry = Convert.ToByte(numberTry);   // Pass build process but app will crash because byte is too small to contain 1234
+                Console.WriteLine(byteNumberTry);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte.");
+                // throw;
+            }
+
         }
     }
 }
