979dd6da16425006d350b2b33a42db00efea2184
Sébastien Jacques <sebastien@X-Wing.local>
2018-05-28 15:09:02 +0200

    Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Enums
+{
+    // Enum = new type => it needs to be defined at namespace level
+    // By default: begin at 0, each value is incremented by 1
+    // Best practice: Always set values of enum
+    public enum ShippingMethod {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var selectedShippingMethod = ShippingMethod.Express;
+            Console.WriteLine((int)selectedShippingMethod);  //  3
+            Console.WriteLine(selectedShippingMethod.ToString());   // Express
+            Console.WriteLine(selectedShippingMethod);  //  Express => Console.WriteLine always used the ToString method
+
+            // Convert int to enum correspondentie
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);    // Express
+
+            // Convert string to enum correspondentie
+            var methodName = "Express";
+            // Parse a string to and object with the correspondance of the shippingMethod then cast it to ShippingMethod type
+            var shippingMethodFromString = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+            Console.WriteLine((int)shippingMethodFromString);    //  3
+        }
+    }
+}
