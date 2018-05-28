adc9997b844763d9128bc7c02a6c28ddc9530bc7
Sébastien Jacques <sebastien@X-Wing.local>
2018-05-28 15:21:45 +0200

    Reference type and value type part 1

---
+++ ReferenceTypesAndValueTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReferenceTypesAndValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = a;
+            b++;    
+            Console.WriteLine(string.Format("a: {0}, b: {1}", a, b)); // int is value type: a = 10 and b = 11
+
+            var array1 = new int[3] {1, 2, 3};
+            var array2 = array1;
+            array2[0] = 0;  
+            Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {0}", array1[0], array2[0])); // array is reference type so both are edited
+        }
+    }
+}
