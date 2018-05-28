55e76a5db55de5651c303b3ca60cbdba9a3bd6b4
Sébastien Jacques <sebastien@X-Wing.local>
2018-05-28 15:28:03 +0200

    Reference type and value type part 2

--- ReferenceTypesAndValueTypes/Program.cs
+++ ReferenceTypesAndValueTypes/Program.cs
@@ -2,6 +2,9 @@
 
 namespace ReferenceTypesAndValueTypes
 {
+    public class Person {
+        public int Age;
+    }
     class Program
     {
         static void Main(string[] args)
@@ -15,6 +18,24 @@ namespace ReferenceTypesAndValueTypes
             var array2 = array1;
             array2[0] = 0;  
             Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {0}", array1[0], array2[0])); // array is reference type so both are edited
+
+            var number = 1;
+            Increment(number);  // number is passed by value
+            Console.WriteLine(number); // 1
+
+            var person = new Person() { Age = 20 };
+            MakeOld(person);    // person is passed by reference
+            Console.WriteLine(person.Age);  // 30
+        }
+
+        // Don't return any value
+        public static void Increment(int number) {
+            number +=10;
+        }
+
+        // Don't return any value
+        public static void MakeOld(Person person) {
+            person.Age +=10;
         }
     }
 }
