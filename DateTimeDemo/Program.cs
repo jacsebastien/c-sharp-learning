cbca0557ad546d5d4e2e3e7f6000461d9e7373cd
Sébastien Jacques <sebastien@X-Wing.local>
2018-05-29 14:35:18 +0200

    DateTime demo

---
+++ DateTimeDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DateTimeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;    // get the date of the moment
+            var today = DateTime.Today;  // Today's date (without time)
+
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString());
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
+        }
+    }
+}
