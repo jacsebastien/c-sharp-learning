ac811fce2a36204b412b27683be09fcef3c2de3e
Sébastien Jacques <sebastien@X-Wing.local>
2018-05-29 17:44:26 +0200

    Strings advances project creation

---
+++ StringsAdvanced/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StringsAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Seb Jcqs ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());   // remove space at begining/end of string
+            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper()); 
+        }
+    }
+}
