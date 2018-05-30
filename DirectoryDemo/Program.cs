078335393d2c65ef000543e5ab6bbd817f82cdea
Sébastien Jacques <sebastien@X-Wing.local>
2018-05-30 16:05:15 +0200

    Directory and DirectoryInfo

---
+++ DirectoryDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string PATH = "/Users/me/www/c-sharp-training";
+
+            // Directory.CreateDirectory(PATH);
+
+            // var files = Directory.GetFiles(PATH, ".cs", SearchOption.AllDirectories); // return all files in the selected directory and sub directories
+            // foreach (var file in files)
+            //     Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(PATH, "*.*", SearchOption.AllDirectories);
+            foreach (var dir in directories)
+                Console.WriteLine(dir);
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
