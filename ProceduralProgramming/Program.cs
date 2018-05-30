2d85d0a43a29ef2ede529800607c9f0ad73dc2c9
Sébastien Jacques <sebastien@X-Wing.local>
2018-05-30 11:49:14 +0200

    Restructure program

--- ProceduralProgramming/Program.cs
+++ ProceduralProgramming/Program.cs
@@ -7,16 +7,22 @@ namespace ProceduralProgramming
         static void Main(string[] args)
         {
             Console.WriteLine("What's your name? ");
-            var name = Console.ReadLine();
 
+            var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+
+            Console.WriteLine("Reversed name: " + reversed);
+        }
+
+        // Main method is static so this one need to be static too to be called in Main method
+        public static string ReverseName(string name) {
             var array = new char[name.Length];
 
             for (int i = name.Length; i > 0; i--) {
                 array[name.Length -i] = name[i - 1];
             }
 
-            var reversed = new string(array);
-            Console.WriteLine("Reversed name: " + reversed);
+            return new string(array);
         }
     }
 }
