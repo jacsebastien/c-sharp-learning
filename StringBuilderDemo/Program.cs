987a2b3ea19703d7dc7e8242839c615a1a1d0cb2
Sébastien Jacques <sebastien@X-Wing.local>
2018-05-30 11:13:12 +0200

    StringBuilder

---
+++ StringBuilderDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder("Hello world !");
+
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10);   // StringBuilder methods return new string so we can chain it
+
+            builder.Replace('-', '+');  // or use it separately
+
+            builder.Remove(0, 10);
+
+            builder.Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First character: " + builder[0]);
+        }
+    }
+}
