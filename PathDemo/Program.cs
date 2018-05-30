e064d87cc056f8d52c4ed8b5f19fb93fcbb614ab
Sébastien Jacques <sebastien@X-Wing.local>
2018-05-30 16:18:23 +0200

    Working with Path

---
+++ PathDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace PathDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "/Users/sebastien/www/c-sharp-training/HelloWorld/HelloWorld.csproj";
+            
+            Console.WriteLine("Path: " + path);
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File name: " + Path.GetFileName(path));
+            Console.WriteLine("File name without extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
+        }
+    }
+}
