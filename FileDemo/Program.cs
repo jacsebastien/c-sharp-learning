187db34f66a1d04dc318d17b215226abd2a54fb4
Sébastien Jacques <sebastien@X-Wing.local>
2018-05-30 12:33:59 +0200

    File and FileInfo Demo

--- FileDemo/Program.cs
+++ FileDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FileDemo
 {
@@ -6,7 +7,30 @@ namespace FileDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var path = "/Users/me/folder1/file.txt";
+            var pathCopy = "/Users/me/folder2/file.txt";
+        
+            // File =>  static methods
+            // For a small number of operations on a file (security checking on each operation)
+            File.Copy(path, pathCopy, true);    // copy and override if exists
+
+            File.Delete(path);
+
+            if(File.Exists(path)) {
+                // Do something
+            }
+
+            var content = File.ReadAllText(path);
+
+            // FileInfo => instance methods
+            // For lot of operations on a file (security checking only on creation)
+            var fileInfo = new FileInfo(path);
+            
+            fileInfo.CopyTo(pathCopy);
+            fileInfo.Delete();
+            if(fileInfo.Exists) {
+                // ...
+            }
         }
     }
 }
