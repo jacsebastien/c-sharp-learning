5b2dfad3005f1159a6e33dd0cf86853d11bfb322
Sébastien Jacques <sebastien@X-Wing.local>
2018-05-30 11:18:32 +0200

    Procedural start

---
+++ ProceduralProgramming/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ProceduralProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name? ");
+            var name = Console.ReadLine();
+
+            var array = new char[name.Length];
+
+            for (int i = name.Length; i > 0; i--) {
+                array[name.Length -i] = name[i - 1];
+            }
+
+            var reversed = new string(array);
+            Console.WriteLine("Reversed name: " + reversed);
+        }
+    }
+}
